64e44b07a3c82177ab95328ec23480dbe7c498f6
Marina0514 <69470207+Marina0514@users.noreply.github.com>
2020-08-25 10:28:24 +0900

    Add files via upload

---
+++ about.js
@@ -0,0 +1,17 @@
+$(function() {
+
+  $('#burger').click(function() {
+  
+  $('.menu-modal').slideDown();
+  $('#burger').hide();
+  
+  });
+  
+  
+  $('.fa-times').click(function() {
+  
+    $('.menu-modal').slideUp();
+    $('#burger').show();
+  });
+  
+  })
